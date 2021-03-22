5c97e6187dd3c586b0029c730ed7db89a41067b6
adr76 <oleksandr.druz@gmail.com>
2021-03-22 16:59:53 +0200

    oled_controller: led keys worked

--- klippy/extras/oled_controller.py
+++ klippy/extras/oled_controller.py
@@ -23,7 +23,7 @@ DEFVALA    = 0x06
 DEFVALB    = 0x07
 INTCONA    = 0x08
 INTCONB    = 0x09
-IOCON      = 0x0A #0x0B is the same
+IOCON      = 0x0A
 GPPUA      = 0x0C
 GPPUB      = 0x0D
 INTFA      = 0x0E
@@ -35,16 +35,26 @@ GPIOB      = 0x13
 OLATA      = 0x14
 OLATB      = 0x15
 
-GPIOA_DEF = 0x5f
-GPIOB_DEF = 0x6d
-GPIO_DEF = 0x5f6d
+DEFA = 0x5f
+DEFB = 0x6d
+DEFAB = 0x6d5f
 
-leds = { # name : [pin, state]
-    'filament': 2, #blue
-    'pwr'     : 13, #red
-    'fan'     : 16, #blue
-    'print'   : 6, #green
-    'pause'   : 8 #yellow
+#       LED1 LED2 LED3 LED4 LED5
+# PORT  9    12   15   5    7  
+#       KEY1 KEY2 KEY3 KEY4 KEY5 KEY6 KEY7 KEY8 KEY9 KEY10 KEY11
+# PORT   8    10   11   0    1    2    3    4    13   14    6
+#
+# BIT     0123 4567   0123 4567
+# PA|PB   1111 1010 | 1011 0110
+# ~PB|PA  0110 1101 | 0101 1111 #0x6d5f
+#         0x6d      | 0x5f
+
+leds = {
+    'filament': 9, #blue
+    'pwr'     : 12, #red
+    'fan'     : 15, #blue
+    'print'   : 5, #green
+    'pause'   : 7 #yellow
 }
 
 # Buttons 
@@ -59,6 +69,7 @@ MCP23017_REPORT_TIME = .5 #sec
 # Klipper Log
 # tail -f /tmp/klippy.log | grep -i -E "(mcp23017:|oled_controller.py|error|Unable)"
 # tail -f /tmp/klippy.log | grep -i -E "(mcp23017:|error|Unable)
+# tail -f /tmp/klippy.log | grep -v Stats
 
 class mcp23017(object):
     def __init__(self, config):
@@ -100,8 +111,11 @@ class mcp23017(object):
     def _init_mcp23017(self):
         try: # Init Registers
             # Port Directions
-            self.write_register(IODIRA, 0x5f) # 0101 1111
-            self.write_register(IODIRB, 0x6d) # 0110 1101
+            self.write_register(IODIRA, 0x5f) # ~1111 1010
+            self.write_register(IODIRB, 0x6d) # ~1011 0110
+            # Leds Off
+            self.write_register(GPIOA, 0x5f)
+            self.write_register(GPIOB, 0x6d)
             # Keys PolUp
             self.write_register(GPPUA, 0x5f)
             self.write_register(GPPUB, 0x6d)
@@ -123,19 +137,10 @@ class mcp23017(object):
             ### Button Press Hook
             btn_name = self.get_btn()
             if btn_name:
-                # Set buttons led
-                if btn_name in leds.keys():
-                    if self.led_state[btn_name]:
-                        state = 0
-                    else:
-                        state = 1
-                    logging.info('mcp23017: set_state %s %d' % (btn_name, state))
-                    self.set_led(btn_name, state)
-                    self.led_state[btn_name] = state
-                # Run button macros
-                # self.gcode.run_script(self.btn_tmpl[btn_name].render())
+                if btn_name in leds.values():
+                    self.set_led(btn_name)
+                self.gcode.run_script(self.btn_tmpl[btn_name].render())
                 self.last_btn = btn_name
-                # logging.info('mcp23017: data %s' % self.data)
         except Exception:
             logging.exception("mcp23017: Error reading data")
             return self.reactor.NEVER
@@ -143,19 +148,32 @@ class mcp23017(object):
         measured_time = self.reactor.monotonic()
         # self._callback(self.mcu.estimated_print_time(measured_time), self.data)
         # logging.info('mcp23017: %d' % measured_time)
-        # self.data = measured_time
         return measured_time + self.report_time
 
     def led_test(self):
         try:
-            for pin in leds.values():
-                logging.info('mcp23017: led test %d', pin)
-                self.set_led(pin, 1)
+            for led in leds.keys():
+                logging.info('mcp23017: Led test %d', led)
+                self.set_led(led)
                 self.reactor.pause(self.reactor.monotonic() + 3) # wait 3s
-                self.set_led(pin, 0)
+                self.set_led(led)
         except Exception:
             logging.exception("mcp23017: Error Led Test")
 
+    def bit_set(self, reg, bit, val):
+        reg_data = self.read_register(reg)
+        if val:
+            reg_data |= (1 << bit)
+        else:
+           reg_data &= ~(1 << bit)
+           
+        self.write_register(reg, reg_data)
+ 
+    def bit_change(self, reg, bit):
+        reg_data = self.read_register(reg)
+        reg_data ^= (1 << bit) 
+        self.write_register(reg, reg_data)  
+           
     def read_register(self, reg_name):
         params = self.i2c.i2c_read([reg_name], 8)
         return bytearray(params['response'])[0]
@@ -179,30 +197,22 @@ class mcp23017(object):
         # logging.info('mcp23017: GPIO %#x' % ab)
         return ab
 
-    def set_led(self, led, on):
-        a = self.read_register(OLATA)
-        b = self.read_register(OLATB)
-        gpio = (a << 8) | b
-        n = leds[led]
-
-        if on:
-            new_gpio = gpio | (1<<n)
+    def set_led(self, led, on=1):
+        bit = leds[led]       
+        state = self.led_state[led]
+        if bit < 8:
+            self.bit_change(GPIOA, bit)
+            # self.bit_set(GPIOA, bit, on)
         else:
-            new_gpio = gpio & ~(1<<n)
-       
-        logging.info('led:  %s %d %d' % (led, on, n))
-        logging.info('old gpio:  %#x %s' % (gpio, bin(gpio)))
-        logging.info('new gpio:  %#x %s' % (gpio, bin(new_gpio)))
-
-        # self.write_gpio(new_gpio)
-
-        ## shell metod
-        # cmd = "gpio -x mcp23017:100:0x20 write "+ str(100 + pin) + " " + str(on)
-        # logging.info('mcp23017: set_led %s', cmd)
-        # subprocess.run(["gpio", "-x", "mcp23017:100:0x20", "write", str(100 + pin), str(on)])
-        # stream = os.popen(cmd)
-        # stream.close()
-    
+            self.bit_change(GPIOB, bit-8) 
+            # self.bit_set(GPIOB, bit-8, on)
+        # Change State
+        if state:
+            state = 0
+        else:       
+            state = 1
+        self.led_state[led] = state
+            
     def write_gpio(self, data):
         a = data >> 8
         b = data & 0xff
